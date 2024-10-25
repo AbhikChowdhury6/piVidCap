20997392ceb06bb4f6e77906029ff8ea6c7599b1
AbhikChowdhury6 <abhik.ch6@gmail.com>
2024-10-24 23:13:00 -0700

    did some multiprocessing with tesseract to try and speed up my ocr, but addeing a few ectra frames to the job made it take 7x as long, going to try and see if keras ocr does the job

--- homeVideo/ocrTest.py
+++ homeVideo/ocrTest.py
@@ -1,6 +1,5 @@
 import cv2
 import pytesseract
-from PIL import Image
 import time
 
 testFile = '/media/chowder/abhiksFiles/personalData/wyzeCams/roomCamBy3dp/20201230/10/20.mp4'
@@ -8,24 +7,35 @@ pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 cap = cv2.VideoCapture(testFile)
 shownTsList = []
 while cap.isOpened():
+    start_time = time.time()
     ret, frame = cap.read()
     if not ret:
         break
 
     print(frame.shape)
-    # Define the region of interest (ROI) to crop
+    # Define the region of interest (ROI) to crop the whole timestamp # 130ms
     x1, y1 = 1350, 1000  # Starting coordinates
     x2, y2 = 1900, 1080  # Ending coordinates
+
+    # Region to crop just the seconds # 100ms
+    # x1, y1 = 1810, 1000  # Starting coordinates
+    # x2, y2 = 1880, 1080  # Ending coordinates
 
     # Crop the frame
     cropped_frame = frame[y1:y2, x1:x2]
 
-    cv2.imshow('cropped image', cropped_frame)
+    #full timestamp downsampled is 115ms and allowing all charecters
+    #also actually works, going to see how multiprocessing might speed this up
+    image_resized = cv2.resize(cropped_frame, (cropped_frame.shape[1] // 2, cropped_frame.shape[0] // 2), interpolation=cv2.INTER_LINEAR)
+    cv2.imshow('resized image', image_resized)
 
 
-    img_rgb = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2RGB)
-    print(pytesseract.image_to_string(img_rgb, lang='eng'))
-    
+
+    img_rgb = cv2.cvtColor(image_resized, cv2.COLOR_BGR2RGB)
+    custom_config = '--psm 6 -l eng'
+    print(pytesseract.image_to_string(img_rgb, config=custom_config))
+    end_time = time.time()
+    print((end_time - start_time) * 1000)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
