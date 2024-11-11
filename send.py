304524945a8d15246ed360a3dacc36034562185c
AbhikChowdhury6 <abhik.ch6@gmail.com>
2024-11-11 15:14:45 -0700

    send.py is working and tested

---
+++ send.py
@@ -0,0 +1,41 @@
+import os
+import subprocess
+import sys
+from datetime import datetime
+import tzlocal
+
+if len(sys.argv) > 1:
+    deviceName = sys.argv[1]
+else: 
+    deviceName = "notSet"
+
+
+pathToCollectedData = "/home/" + os.getlogin() + "/Documents/collectedData/"
+
+foldersInCollectedData = os.listdir(pathToCollectedData)
+nameOfTodaysFolder = deviceName + "-" + datetime.now(tzlocal.get_localzone()).strftime("%Y-%m-%d%z")
+for folderName in foldersInCollectedData:
+    if folderName == nameOfTodaysFolder:
+        continue
+    source = pathToCollectedData + folderName
+    
+    # send the folder over
+    print("starting send")
+    o = subprocess.run(["scp", "-r", source, "uploadingGuest@192.168.1.242:/home/uploadingGuest/recentCaptures/"],
+                         capture_output=True)
+    print(f"the returncode for uploading the direcotry was {o.returncode}")
+    
+    # make it writeable by other users since the umask in the .bashrc isn't working for some reason
+    o2 = subprocess.run(["ssh", "uploadingGuest@192.168.1.242", "chmod", "-R", "777", 
+                        "/home/uploadingGuest/recentCaptures/" + folderName + "/"], 
+                        capture_output=True)
+    print(f"the returncode for upating the permissions was {o2.returncode}")
+
+    #delete the folder locally if the send was successful
+    if o.returncode == 0:
+        print(f"successfuly sent now deleting {source}")
+        o = subprocess.run(["rm", "-r", source], capture_output=True)
+        print("deleted") if o.returncode == 0 else print(o)
+    else:
+        print(f"there was a problem sending {source} not deleting")
+        print(o)
