a32b11c119e05fc58357f51108a60ad395932bf4
AbhikChowdhury6 <abhik.ch6@gmail.com>
2025-03-06 00:35:39 -0700

    had a recursive loop in my circular buffer

--- circularTimeSeriesBuffer.py
+++ circularTimeSeriesBuffer.py
@@ -15,13 +15,9 @@ class CircularTimeSeriesBuffer:
     def __setitem__(self, index, value):
         """Set value and timestamp at a circular index."""
         index = index % self.size  # Ensure circular indexing
-        if index == self.nextidx:
-            self.append(value[0], value[1])
-        else:
-            self.data_buffer[index] = value[0]  # Assume value is a tuple (data, timestamp)
-            self.time_buffer[index] = int(value[1].replace(tzinfo=timezone.utc).timestamp() * 1e9 + value[1].microsecond * 1e3)
-    
-
+        self.data_buffer[index] = value[0]  # Assume value is a tuple (data, timestamp)
+        self.time_buffer[index] = int(value[1].replace(tzinfo=timezone.utc).timestamp() * 1e9 + value[1].microsecond * 1e3)
+            
     def __getitem__(self, index):
         """Retrieve (value, timestamp) from a circular index."""
         index = index % self.size  # Ensure circular indexing
