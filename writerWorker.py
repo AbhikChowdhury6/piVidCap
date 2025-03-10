32d9f3a46e2c2be4750ebf641d6b0c9117e32a5c
AbhikChowdhury6 <abhik.ch6@gmail.com>
2025-03-10 08:41:29 -0700

    added another debug line

--- writerWorker.py
+++ writerWorker.py
@@ -162,6 +162,7 @@ def writer_worker(ctsb: CircularTimeSeriesBuffers, personSignal, exitSignal):
                 for frame in ctsb.data_buffers[bufferNum][:ctsb.lengths[bufferNum][0]]:
                     frame = frame.cpu().numpy()  # Convert from torch tensor to numpy
                     frame = frame.astype(np.uint8)
+                    print(f"writer: frame shape: {frame.shape}, dtype: {frame.dtype}")
                     success = output.write(frame)
                     if not success:
                         print(f"writer: Failed to write frame")
